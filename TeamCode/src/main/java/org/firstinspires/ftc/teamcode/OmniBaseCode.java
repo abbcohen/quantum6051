95f3caa8bc514649d1d1fdb1d96caf2ec9d0bf36
acohen <c19ac@daltong.org>
2017-10-10 14:32:24 -0400

    omni with servos (not finished / not in functions)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniBaseCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniBaseCode.java
@@ -1,19 +1,16 @@
 
-
 package org.firstinspires.ftc.teamcode;
-
-        import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
-        import com.qualcomm.robotcore.util.ElapsedTime;
-        import com.qualcomm.robotcore.util.Range;
-
-        import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-
-@TeleOp(name="OMNI BOI", group="Iterative Opmode")
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+@TeleOp(name="Omni with servos", group="Iterative Opmode")
 public class OmniBaseCode extends OpMode
 {
     // Declare OpMode members.
@@ -22,14 +19,16 @@ public class OmniBaseCode extends OpMode
     private DcMotor WheelTwo = null;
     private DcMotor WheelThree = null;
     private DcMotor WheelZero = null;
-
+    private CRServo ServoFour = null;
+    private CRServo ServoFive = null;
+    private double moveSpeed = .75;
+    private double turnSpeed = .5;
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
-
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
@@ -37,18 +36,20 @@ public class OmniBaseCode extends OpMode
         WheelTwo  = hardwareMap.get(DcMotor.class, "WheelTwo");
         WheelThree = hardwareMap.get(DcMotor.class, "WheelThree");
         WheelZero = hardwareMap.get(DcMotor.class, "WheelZero");
-
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         WheelOne.setDirection(DcMotor.Direction.FORWARD);
         WheelTwo.setDirection(DcMotor.Direction.REVERSE);
         WheelThree.setDirection(DcMotor.Direction.REVERSE);
         WheelZero.setDirection(DcMotor.Direction.FORWARD);
-
+        //Servo initialization
+        ServoFour = hardwareMap.get(CRServo.class, "ServoFour");
+        ServoFive = hardwareMap.get(CRServo.class, "ServoFive");
+        ServoFour.setDirection(CRServo.Direction.REVERSE);
+        ServoFive.setDirection(CRServo.Direction.FORWARD);
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
     }
-
     /*
      * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
      */
@@ -59,7 +60,6 @@ public class OmniBaseCode extends OpMode
         WheelThree.setPower(0);
         WheelZero.setPower(0);
     }
-
     /*
      * Code to run ONCE when the driver hits PLAY
      */
@@ -72,76 +72,95 @@ public class OmniBaseCode extends OpMode
      */
     @Override
     public void loop() {
-
-
-//MOVING (NOT TURNING) CODE
+//MOVING CODE
         //move forward
-        if (gamepad1.left_stick_y < -.1) {
-            WheelOne.setPower(gamepad1.left_stick_y);
-            WheelTwo.setPower(gamepad1.left_stick_y);
-            WheelThree.setPower(gamepad1.left_stick_y);
-            WheelZero.setPower(gamepad1.left_stick_y);
+        if (gamepad1.left_stick_y < -1/(10*moveSpeed))
+        {
+            WheelOne.setPower(gamepad1.left_stick_y*moveSpeed);
+            WheelTwo.setPower(gamepad1.left_stick_y*moveSpeed);
+            WheelThree.setPower(gamepad1.left_stick_y*moveSpeed);
+            WheelZero.setPower(gamepad1.left_stick_y*moveSpeed);
         }
-
         //move backward
-        else if (gamepad1.left_stick_y > .1) {
-            WheelOne.setPower(gamepad1.left_stick_y);
-            WheelTwo.setPower(gamepad1.left_stick_y);
-            WheelThree.setPower(gamepad1.left_stick_y);
-            WheelZero.setPower(gamepad1.left_stick_y);
+        else if (gamepad1.left_stick_y > 1/(10*moveSpeed))
+        {
+            WheelOne.setPower(gamepad1.left_stick_y*moveSpeed);
+            WheelTwo.setPower(gamepad1.left_stick_y*moveSpeed);
+            WheelThree.setPower(gamepad1.left_stick_y*moveSpeed);
+            WheelZero.setPower(gamepad1.left_stick_y*moveSpeed);
         }
         //move left
-        if (gamepad1.left_stick_x < -.1) {
-            WheelOne.setPower(gamepad1.left_stick_x);
-            WheelTwo.setPower(gamepad1.left_stick_x);
-            WheelThree.setPower(-gamepad1.left_stick_x);
-            WheelZero.setPower(-gamepad1.left_stick_x);
+        if (gamepad1.left_stick_x < -1/(10*moveSpeed))
+        {
+            WheelOne.setPower(-gamepad1.left_stick_x*moveSpeed);
+            WheelTwo.setPower(-gamepad1.left_stick_x*moveSpeed);
+            WheelThree.setPower(gamepad1.left_stick_x*moveSpeed);
+            WheelZero.setPower(gamepad1.left_stick_x*moveSpeed);
         }
         //move right
-        else if (gamepad1.left_stick_x > .1) {
-            telemetry.addLine("BEN CW");
-            WheelOne.setPower(gamepad1.left_stick_x);
-            WheelTwo.setPower(gamepad1.left_stick_x);
-            WheelThree.setPower(-gamepad1.left_stick_x);
-            WheelZero.setPower(-gamepad1.left_stick_x);
+        else if (gamepad1.left_stick_x > 1/(10*moveSpeed))
+        {
+            WheelOne.setPower(-gamepad1.left_stick_x*moveSpeed);
+            WheelTwo.setPower(-gamepad1.left_stick_x*moveSpeed);
+            WheelThree.setPower(gamepad1.left_stick_x*moveSpeed);
+            WheelZero.setPower(gamepad1.left_stick_x*moveSpeed);
         }
-        //nothing
-        else{
+        //turn counter-clockwise
+        if (gamepad1.right_stick_x < -1/(10*turnSpeed))
+        {
+            WheelOne.setPower(gamepad1.right_stick_x*turnSpeed);
+            WheelTwo.setPower(-gamepad1.right_stick_x*turnSpeed);
+            WheelThree.setPower(-gamepad1.right_stick_x*turnSpeed);
+            WheelZero.setPower(gamepad1.right_stick_x*turnSpeed);
+        }
+        //turn clockwise
+        else if (gamepad1.right_stick_x > 1/(10*turnSpeed))
+        {
+            WheelOne.setPower(gamepad1.right_stick_x*turnSpeed);
+            WheelTwo.setPower(-gamepad1.right_stick_x*turnSpeed);
+            WheelThree.setPower(-gamepad1.right_stick_x*turnSpeed);
+            WheelZero.setPower(gamepad1.right_stick_x*turnSpeed);
+        }
+        //stop motors
+        else
+        {
             WheelOne.setPower(0);
             WheelTwo.setPower(0);
             WheelThree.setPower(0);
             WheelZero.setPower(0);
         }
-        telemetry.addData("leftsticky", gamepad1.left_stick_y);
-        telemetry.addData("leftstickx", gamepad1.left_stick_x);
+        //grabber in
+        if (gamepad1.right_trigger > .1)
+        {
+            ServoFour.setPower(-gamepad1.right_trigger);
+            ServoFive.setPower(-gamepad1.right_trigger);
+        }
+        //grabber out
+        else if (gamepad1.left_trigger > .1)
+        {
+            ServoFour.setPower(gamepad1.left_trigger);
+            ServoFive.setPower(gamepad1.left_trigger);
+        }
+        //stop servos
+        else
+        {
+            ServoFour.setPower(0);
+            ServoFive.setPower(0);
+        }
+        telemetry.addData("left_stick_y", gamepad1.left_stick_y);
+        telemetry.addData("left_stick_x", gamepad1.left_stick_x);
+        telemetry.addData("right_stick_y", gamepad1.right_stick_y);
+        telemetry.addData("right_stick_x", gamepad1.right_stick_x);
+        telemetry.addData("x_button", gamepad1.x);
+        telemetry.addData("b_button", gamepad1.b);
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-
-
-        //turn left
-        if (gamepad1.right_stick_x < -.1) {
-            WheelOne.setPower(-gamepad1.left_stick_x);
-            WheelTwo.setPower(-gamepad1.left_stick_x);
-            WheelThree.setPower(-gamepad1.left_stick_x);
-            WheelZero.setPower(-gamepad1.left_stick_x);
-        }
-        //turn right
-        else if (gamepad1.right_stick_x > .1) {
-            WheelOne.setPower(gamepad1.left_stick_x);
-            WheelTwo.setPower(gamepad1.left_stick_x);
-            WheelThree.setPower(gamepad1.left_stick_x);
-            WheelZero.setPower(gamepad1.left_stick_x);
-        }
-
-
     }
-
     /*
      * Code to run ONCE after the driver hits STOP
      */
     @Override
     public void stop() {
     }
-
 }
