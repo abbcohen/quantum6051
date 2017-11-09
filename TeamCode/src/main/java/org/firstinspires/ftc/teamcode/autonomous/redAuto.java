204dfdfd6ec31739f2ea6e871a9f8b3ad0e68c23
acohen <c19ac@daltong.org>
2017-11-09 18:10:15 -0500

    WE EATIN

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/redAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/redAuto.java
@@ -2,13 +2,18 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 import android.graphics.Color;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.CRServo;
-@TeleOp(name="Omni with servos", group="Iterative Opmode")
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import static java.lang.Thread.sleep;
+
+@Autonomous(name="Red Juul Auto", group="Iterative Opmode")
 public class redAuto extends OpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -16,10 +21,14 @@ public class redAuto extends OpMode {
     private DcMotor WheelTwo = null;
     private DcMotor WheelThree = null;
     private DcMotor WheelZero = null;
-    private CRServo ServoFour = null;
-    private CRServo ServoFive = null;
+    private DcMotor LiftMotor = null;
+    private Servo GlyphServo = null;
+    private Servo JewelServo = null;
     private double moveSpeed = .75;
     private double turnSpeed = .5;
+    private double liftSpeed = 1;
+    private boolean slomo;
+    public double servoPos = .5;
     private ColorSensor colorSensor;
     /*
      * Code to run ONCE when the driver hits INIT
@@ -34,19 +43,24 @@ public class redAuto extends OpMode {
         WheelTwo  = hardwareMap.get(DcMotor.class, "WheelTwo");
         WheelThree = hardwareMap.get(DcMotor.class, "WheelThree");
         WheelZero = hardwareMap.get(DcMotor.class, "WheelZero");
+        LiftMotor = hardwareMap.get(DcMotor.class, "LiftMotor");
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         WheelOne.setDirection(DcMotor.Direction.FORWARD);
         WheelTwo.setDirection(DcMotor.Direction.REVERSE);
         WheelThree.setDirection(DcMotor.Direction.REVERSE);
         WheelZero.setDirection(DcMotor.Direction.FORWARD);
+        LiftMotor.setDirection(DcMotor.Direction.REVERSE);
         //Servo initialization
-        ServoFour = hardwareMap.get(CRServo.class, "ServoFour");
-        ServoFive = hardwareMap.get(CRServo.class, "ServoFive");
-        ServoFour.setDirection(CRServo.Direction.REVERSE);
-        ServoFive.setDirection(CRServo.Direction.FORWARD);
+        GlyphServo = hardwareMap.get(Servo.class, "GlyphServo");
+        GlyphServo.setDirection(Servo.Direction.REVERSE);
+        GlyphServo.scaleRange(0,2);
+        JewelServo = hardwareMap.get(Servo.class, "JewelServo");
+        JewelServo.setDirection(Servo.Direction.REVERSE);
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
 
-        colorSensor = hardwareMap.colorSensor.get("color sensor");
+        colorSensor = hardwareMap.colorSensor.get("colorSensor");
         boolean bLedOn = true;
         colorSensor.enableLed(bLedOn);
         // Tell the driver that initialization is complete.
@@ -57,58 +71,59 @@ public class redAuto extends OpMode {
      * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
      */
     public void moveForward() {
-        WheelOne.setPower(gamepad1.left_stick_y*moveSpeed);
-        WheelTwo.setPower(gamepad1.left_stick_y*moveSpeed);
-        WheelThree.setPower(gamepad1.left_stick_y*moveSpeed);
-        WheelZero.setPower(gamepad1.left_stick_y*moveSpeed);
+        WheelOne.setPower(.25);
+        WheelTwo.setPower(.25);
+        WheelThree.setPower(.25);
+        WheelZero.setPower(.25);
     }
     public void moveBackward() {
-        WheelOne.setPower(gamepad1.left_stick_y*moveSpeed);
-        WheelTwo.setPower(gamepad1.left_stick_y*moveSpeed);
-        WheelThree.setPower(gamepad1.left_stick_y*moveSpeed);
-        WheelZero.setPower(gamepad1.left_stick_y*moveSpeed);
+        WheelOne.setPower(-.25);
+        WheelTwo.setPower(-.25);
+        WheelThree.setPower(-.25);
+        WheelZero.setPower(-.25);
     }
     public void moveLeft() {
-        WheelOne.setPower(-gamepad1.left_stick_x*moveSpeed);
-        WheelTwo.setPower(-gamepad1.left_stick_x*moveSpeed);
-        WheelThree.setPower(gamepad1.left_stick_x*moveSpeed);
-        WheelZero.setPower(gamepad1.left_stick_x*moveSpeed);
+        WheelOne.setPower(.25);
+        WheelTwo.setPower(.25);
+        WheelThree.setPower(-.25);
+        WheelZero.setPower(-.25);
     }
     public void moveRight() {
-        WheelOne.setPower(-gamepad1.left_stick_x*moveSpeed);
-        WheelTwo.setPower(-gamepad1.left_stick_x*moveSpeed);
-        WheelThree.setPower(gamepad1.left_stick_x*moveSpeed);
-        WheelZero.setPower(gamepad1.left_stick_x*moveSpeed);
+        WheelOne.setPower(-.25);
+        WheelTwo.setPower(-.25);
+        WheelThree.setPower(.25);
+        WheelZero.setPower(.25);
     }
     public void turnClockwise() {
-        WheelOne.setPower(gamepad1.right_stick_x*turnSpeed);
-        WheelTwo.setPower(-gamepad1.right_stick_x*turnSpeed);
-        WheelThree.setPower(-gamepad1.right_stick_x*turnSpeed);
-        WheelZero.setPower(gamepad1.right_stick_x*turnSpeed);
+        WheelOne.setPower(.25);
+        WheelTwo.setPower(-.25);
+        WheelThree.setPower(-.25);
+        WheelZero.setPower(.25);
     }
     public void turnCounterClockwise() {
-        WheelOne.setPower(gamepad1.right_stick_x*turnSpeed);
-        WheelTwo.setPower(-gamepad1.right_stick_x*turnSpeed);
-        WheelThree.setPower(-gamepad1.right_stick_x*turnSpeed);
-        WheelZero.setPower(gamepad1.right_stick_x*turnSpeed);
+        WheelOne.setPower(-.25);
+        WheelTwo.setPower(.25);
+        WheelThree.setPower(.25);
+        WheelZero.setPower(-.25);
     }
     public void driveStop(){
         WheelOne.setPower(0);
         WheelTwo.setPower(0);
         WheelThree.setPower(0);
         WheelZero.setPower(0);
+    }public void grabber(double pos) {
+        if(pos==1){
+            //OUT left
+            GlyphServo.setPosition(.41);
+        }
+        else if (pos==0) {
+            //IN right
+            GlyphServo.setPosition(.54);
+        }
     }
-    public void grabberIn(){
-        ServoFour.setPower(-gamepad1.right_trigger);
-        ServoFive.setPower(-gamepad1.right_trigger);
-    }
-    public void grabberOut(){
-        ServoFour.setPower(gamepad1.left_trigger);
-        ServoFive.setPower(gamepad1.left_trigger);
-    }
-    public void grabberStop(){
-        ServoFour.setPower(0);
-        ServoFive.setPower(0);
+    public void jewel(char position){
+        if(position == 'u') JewelServo.setPosition(90);
+        if(position =='d') JewelServo.setPosition(0);
     }
 
 
@@ -135,8 +150,9 @@ public class redAuto extends OpMode {
     public void loop() {
        //do stuff here
         // convert the RGB values to HSV values.
-        int red=0;
-        int blue=0;
+
+
+
         float hsvValues[] = {0F, 0F, 0F};
         Color.RGBToHSV(colorSensor.red(), colorSensor.green(), colorSensor.blue(), hsvValues);
 
@@ -147,11 +163,35 @@ public class redAuto extends OpMode {
         telemetry.addData("Blue ", colorSensor.blue());
         telemetry.addData("Hue", hsvValues[0]);
 
-        Color.RGBToHSV(colorSensor.red() * 255/800, colorSensor.green() * 255/800, colorSensor.blue() * 255/800, hsvValues);
-            if (hsvValues[0] > 140 && hsvValues[0] < 310) {
 
-
+        int red=0;
+        int blue=0;
+        for (int x = 0; x < 100; x++) {
+            if(colorSensor.red()>colorSensor.blue()){
+                red++;
             }
+            else {
+                blue++;
+            }
+        }
+
+        double initialtime =  getRuntime();
+
+        if(red>blue){
+            while(getRuntime() < (initialtime + 1.5)) {
+                turnClockwise();
+            }
+        } else{
+            while(getRuntime() < (initialtime + 1.5)) {
+                turnCounterClockwise();
+            }
+        }
+
+//        Color.RGBToHSV(colorSensor.red() * 255/800, colorSensor.green() * 255/800, colorSensor.blue() * 255/800, hsvValues);
+//            if (hsvValues[0] > 140 && hsvValues[0] < 310) {
+//
+//
+//            }
     }
     /*
      * Code to run ONCE after the driver hits STOP
