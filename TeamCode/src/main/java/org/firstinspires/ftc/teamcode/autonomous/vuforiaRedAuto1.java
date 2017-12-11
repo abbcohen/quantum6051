92b2a633d45c48ae638b376f01cfd81524023577
acohen <c19ac@daltong.org>
2017-12-11 13:15:48 -0500

    fixed red vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/vuforiaRedAuto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/vuforiaRedAuto1.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
 
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -17,6 +19,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.CRServo;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 
@@ -131,110 +139,113 @@ public class vuforiaRedAuto1 extends LinearOpMode {
         if (red > blue) {
             telemetry.addData("Red Wins!", colorSensor.red());
             telemetry.update();
-            moveTime(6,.15);
+            moveTime(6, .15);
         } else {
             telemetry.addData("Blue Wins!", colorSensor.red());
             telemetry.update();
-            moveTime(5,.15);
+            moveTime(5, .15);
         }
         JewelServo.setPosition(0);
 
         //turn back to initial position
-        if(red>blue) {
-            moveTime(5,.15);
-        } else if(blue>red) {
-            moveTime(6,.15);
+        if (red > blue) {
+            moveTime(5, .15);
+        } else if (blue > red) {
+            moveTime(6, .15);
         }
 
+        column=getPicto();
+        
         //MOVE TO THE CORRECT COLUMN
         if (column == RelicRecoveryVuMark.CENTER || column == RelicRecoveryVuMark.UNKNOWN) {
-            moveTime(4,1.761); //fill w center value
+            moveTime(4, 1.761); //fill w center value
         } else if (column == RelicRecoveryVuMark.LEFT) {
-            moveTime(4,1.661); //fill w left value
+            moveTime(4, 1.661); //fill w left value
         } else if (column == RelicRecoveryVuMark.RIGHT) {
-            moveTime(4,1.861);//fill w right value
+            moveTime(4, 1.861);//fill w right value
         }
 
         //turn to face cryptobox
-        moveTime(5,1.614);
+        moveTime(5, 1.614);
 
         //move forward
-        moveTime(1,1.2);
+        moveTime(1, 1.2);
 
         //pause
-        moveTime(0,1);
+        moveTime(0, 1);
 
         //release glyph
         moveTime(8, .28);
 
         //pause
-        moveTime(0,1);
+        moveTime(0, 1);
 
         //move back
-        moveTime(2,.25);
+        moveTime(2, .25);
 
         //push back in
-        moveTime(1,.3);
+        moveTime(1, .3);
 
         //move back out
-        moveTime(2,.25);
+        moveTime(2, .25);
 
 
     }
-    public void moveTime(int dir, double time){
+
+    public void moveTime(int dir, double time) {
         double startTime = 0;
-        if(dir == 0){
+        if (dir == 0) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 driveStop();
             }
         }
-        if(dir == 1){
+        if (dir == 1) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 moveForward();
             }
 
         }
-        if(dir == 2){
+        if (dir == 2) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 moveBackward();
             }
         }
-        if(dir == 3){
+        if (dir == 3) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 moveLeft();
             }
         }
-        if(dir == 4){
+        if (dir == 4) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 moveRight();
             }
         }
-        if(dir == 5){
+        if (dir == 5) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 turnClockwise();
             }
         }
-        if(dir == 6){
+        if (dir == 6) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 turnCounterClockwise();
             }
         }
-        if(dir == 7){
+        if (dir == 7) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 succ();
             }
         }
-        if(dir == 8){
+        if (dir == 8) {
             startTime = getRuntime();
-            while(getRuntime() < startTime + time){
+            while (getRuntime() < startTime + time) {
                 blow();
             }
         }
@@ -244,54 +255,64 @@ public class vuforiaRedAuto1 extends LinearOpMode {
         GlyphWheel1.setPower(.6);
         GlyphWheel2.setPower(.6);
     }
+
     public void nosucc() {
         GlyphWheel1.setPower(0);
         GlyphWheel2.setPower(0);
     }
+
     public void blow() {
         GlyphWheel1.setPower(-.6);
         GlyphWheel2.setPower(-.6);
     }
+
     public void grabber(double pos) {
         GlyphServo1.setPosition(pos);
         GlyphServo2.setPosition(pos);
     }
+
     public void moveForward() {
         WheelOne.setPower(-moveSpeed);
         WheelTwo.setPower(-moveSpeed);
         WheelThree.setPower(-moveSpeed);
         WheelZero.setPower(-moveSpeed);
     }
+
     public void moveBackward() {
         WheelOne.setPower(moveSpeed);
         WheelTwo.setPower(moveSpeed);
         WheelThree.setPower(moveSpeed);
         WheelZero.setPower(moveSpeed);
     }
+
     public void moveLeft() {
         WheelOne.setPower(moveSpeed);
         WheelTwo.setPower(moveSpeed);
         WheelThree.setPower(-moveSpeed);
         WheelZero.setPower(-moveSpeed);
     }
+
     public void moveRight() {
         WheelOne.setPower(-moveSpeed);
         WheelTwo.setPower(-moveSpeed);
         WheelThree.setPower(moveSpeed);
         WheelZero.setPower(moveSpeed);
     }
+
     public void turnClockwise() {
         WheelOne.setPower(turnSpeed);
         WheelTwo.setPower(-turnSpeed);
         WheelThree.setPower(-turnSpeed);
         WheelZero.setPower(turnSpeed);
     }
+
     public void turnCounterClockwise() {
         WheelOne.setPower(-turnSpeed);
         WheelTwo.setPower(turnSpeed);
         WheelThree.setPower(turnSpeed);
         WheelZero.setPower(-turnSpeed);
     }
+
     public void driveStop() {
         WheelOne.setPower(0);
         WheelTwo.setPower(0);
@@ -318,5 +339,59 @@ public class vuforiaRedAuto1 extends LinearOpMode {
         while (getRuntime() < startTime + time) {
             LiftMotor.setPower(0);
         }
+    }
+
+    public RelicRecoveryVuMark getPicto() {
+        OpenGLMatrix lastLocation = null;
+
+        /**
+         * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+         * localization engine.
+         */
+        VuforiaLocalizer vuforia;
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        // OR...  Do Not Activate the Camera Monitor View, to save power
+        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        /*
+         * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+         * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+         * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+         * web site at https://developer.vuforia.com/license-manager.
+         *
+         * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+         * random data. As an example, here is a example of a fragment of a valid key:
+         *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+         * Once you've obtained a license key, copy the string from the Vuforia web site
+         * and paste it in to your code onthe next line, between the double quotes.
+         */
+        parameters.vuforiaLicenseKey = "Aeba4Qn/////AAAAGahNOxzreUE8nItPWzsrOlF7uoyrR/qbjue3kUmhxZcfZMSd5MRyEY+3uEoVA+gpQGz5KyP3wEjBxSOAb4+FBYMZ+QblFU4byMG4+aiI+GeeBA+RatQXVzSduRBdniCW4qehTnwS204KTUMXg1ioPvUlbYQmqM5aPMx/2xnYN1b+htNBWV0Bc8Vkyspa0NNgz7PzF1gozlCIc9FgzbzNYoOMhqSG+jhKf47SZQxD6iEAtj5iAkWBvJwFDLr/EfDfPr3BIA6Cpb4xaDc0t4Iz5wJ/p4oLRiEJaLoE/noCcWFjLmPcw9ccwYXThtjC+7u0DsMX+r+1dMikBCZCWWkLzEyjWzy3pOOR3exNRYGZ0vzr";
+
+        /*
+         * We also indicate which camera on the RC that we wish to use.
+         * Here we chose the back (HiRes) camera (for greater range), but
+         * for a competition robot, the front camera might be more convenient.
+         */
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        /**
+         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
+         * in this data set: all three of the VuMarks in the game were created from this one template,
+         * but differ in their instance id information.
+         * @see VuMarkInstanceId
+         */
+        VuforiaTrackables relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        relicTrackables.activate();
+
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        double startTime = getRuntime();
+        while (getRuntime() - startTime < 3) {
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        }
+        return vuMark;
     }
 }
